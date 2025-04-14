a0d1f1af03af904960c88b9d9183cab1e27f6f3b
Prashant-Jhim-2 <prashantjhim2023@gmail.com>
2025-04-14 19:49:31 -0400

    Changes

--- app/home/ClientSide.js
+++ app/home/ClientSide.js
@@ -260,7 +260,7 @@ const page = ({carddata}) =>{
           {Cards.map((data) => {
             if (data != undefined){
               const condition = Enrolled.includes(data.id);
-             
+               console.log(Enrolled , data.id )
               const verify = Details.id == data.ProfessorId
               console.log(`Name : ${data.Name}`)
               console.log(`Already Enrolled ${condition} and Verify ${verify}`)
